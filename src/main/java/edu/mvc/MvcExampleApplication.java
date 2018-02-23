607c2cd060bbdd86e06b6dd2e3ce3491aaac7d66
Andrew <asmal95@mail.ru>
2018-02-24 01:08:58 +0400

    Initial commit of sample Spring MVC application.

---
+++ src/main/java/edu/mvc/MvcExampleApplication.java
@@ -0,0 +1,12 @@
+package edu.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcExampleApplication.class, args);
+	}
+}
