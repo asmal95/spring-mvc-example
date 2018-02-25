6f79237ea7201850a2928bfca193e45dab859892
Andrew <asmal95@mail.ru>
2018-02-25 14:40:39 +0400

    Add template and styles.

--- src/main/java/edu/mvc/controller/IndexController.java
+++ src/main/java/edu/mvc/controller/IndexController.java
@@ -2,8 +2,10 @@ package edu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
     @GetMapping
