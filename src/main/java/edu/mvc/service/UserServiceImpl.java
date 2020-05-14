6ef30132b29524da085e79b5a1add189756b5e93
asmal95 <asmal95@mail.ru>
2020-05-15 03:32:20 +0400

    Fix terrible mistake for field injection

--- src/main/java/edu/mvc/service/UserServiceImpl.java
+++ src/main/java/edu/mvc/service/UserServiceImpl.java
@@ -33,8 +33,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Autowired
-    public PasswordEncoder getPasswordEncoder() {
-        return passwordEncoder;
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
